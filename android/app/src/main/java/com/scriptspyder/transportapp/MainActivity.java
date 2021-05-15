52d9b1ccec32c47a02052a9c574fb68974bcc674
Aashan Ghimire <ashanghimire10@gmail.com>
2021-05-15 14:09:39 +0545

    change app name and add icon assets

--- android/app/src/main/java/com/scriptspyder/transportapp/MainActivity.java
+++ android/app/src/main/java/com/scriptspyder/transportapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.transportapp;
+package com.scriptspyder.transportapp;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "transportapp";
+    return "JMRTransport";
   }
 }
--- android/app/src/main/java/com/scriptspyder/transportapp/MainApplication.java
+++ android/app/src/main/java/com/scriptspyder/transportapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.transportapp;
+package com.scriptspyder.transportapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -62,7 +62,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.transportapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.scriptspyder.transportapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
