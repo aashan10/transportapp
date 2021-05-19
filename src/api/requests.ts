c39a9b31b89a27f92938f8d1e4f9cc39282645ec
Aashan Ghimire <ashanghimire10@gmail.com>
2021-05-19 19:02:10 +0545

    change requests

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -20,6 +20,46 @@ import {ImageOrVideo} from 'react-native-image-crop-picker';
 import {requestLocationPermission} from '../helpers/functions';
 import Geolocation from '@react-native-community/geolocation';
 
+export const get = async (url: string, auth: boolean = true) => {
+  const payload = {
+    headers: getHeaders({'Content-Type': 'application/json'}),
+    method: 'GET',
+  };
+  if (auth) {
+    payload.headers = getHeaders({
+      ...payload.headers,
+      'auth-token': sharedData.user.token,
+    });
+  }
+
+  const response = await fetch(getUrl(url), payload);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Exception(response);
+};
+
+export const post = async (url: string, data: any, auth: boolean = true) => {
+  const payload = {
+    headers: getHeaders({'Content-Type': 'application/json'}),
+    method: 'POST',
+    body: JSON.stringify(data),
+  };
+
+  if (auth) {
+    payload.headers = getHeaders({
+      ...payload.headers,
+      'auth-token': sharedData.user.token,
+    });
+  }
+
+  const response = await fetch(getUrl(url), payload);
+  if (response.ok) {
+    return await response.json();
+  }
+  throw new Exception(response);
+};
+
 export const getUrl = (path: string) => {
   return BASE_URL + path;
 };
@@ -31,27 +71,11 @@ const getHeaders = (headers: {[text: string]: string}) => {
   };
 };
 
-export const userLogin = async ({
-  username,
-  password,
-}: {
+export const userLogin = async (credentials: {
   username: string;
   password: string;
 }) => {
-  const response = await fetch(getUrl(USER_LOGIN), {
-    method: 'POST',
-    headers: getHeaders({}),
-    body: JSON.stringify({
-      phoneNumber: username,
-      password: password,
-    }),
-  });
-
-  if (response.ok) {
-    return await response.json();
-  } else {
-    throw new Exception(response);
-  }
+  return await post(USER_LOGIN, credentials, false);
 };
 
 export const userInfo = async (token: string) => {
@@ -76,64 +100,19 @@ export const createNewItemRequest = async (props: {
   type: string;
   size: string;
 }) => {
-  const response = await fetch(getUrl(VENDOR_ITEM_UPLOAD), {
-    headers: getHeaders({
-      'auth-token': sharedData.user.token,
-    }),
-    method: 'POST',
-    body: JSON.stringify({
-      latitudeOfDeliveryFrom: props.latitude,
-      longitudeOfDeliveryFrom: props.longitude,
-      itemName: props.name,
-      deliveryFrom: props.from,
-      deliveryTo: props.to,
-      quantity: props.quantity,
-      deliveryPriceByVendor: props.price,
-      containerType: props.type,
-      containerSize: props.size,
-    }),
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await post(VENDOR_ITEM_UPLOAD, props);
 };
 
 export const getDriverFeeds = async () => {
-  const response = await fetch(getUrl(DRIVER_EXPLORE), {
-    headers: getHeaders({'auth-token': sharedData.user.token}),
-    method: 'GET',
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await get(DRIVER_EXPLORE);
 };
 
 export const getVendorItemsDetail = async () => {
-  const response = await fetch(getUrl(VENDOR_ITEM_DETAIL), {
-    headers: getHeaders({'auth-token': sharedData.user.token}),
-    method: 'GET',
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await get(VENDOR_ITEM_DETAIL);
 };
 
 export const getDriverItemsDetail = async () => {
-  const response = await fetch(getUrl(DRIVER_ITEM_ACCEPTED_LIST), {
-    headers: getHeaders({'auth-token': sharedData.user.token}),
-    method: 'GET',
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await get(DRIVER_ITEM_ACCEPTED_LIST);
 };
 
 export const registerDriver = async (props: {
@@ -206,59 +185,22 @@ export const registerVendor = async (data: {
   email: string;
   phoneNumber: string;
 }) => {
-  const response = await fetch(getUrl(VENDOR_REGISTER), {
-    headers: getHeaders({'auth-token': sharedData.user.token}),
-    body: JSON.stringify(data),
-    method: 'POST',
-  });
-
-  if (response.ok) {
-    return response;
-  } else {
-    throw new Exception(response);
-  }
+  return await post(VENDOR_REGISTER, data);
 };
 
 export const acceptDeliveryRequest = async (payload: {
   itemId: string;
   vendorId: string;
 }) => {
-  const response = await fetch(getUrl(DRIVER_DELIVERY_ACCEPT), {
-    headers: getHeaders({'auth-token': sharedData.user.token}),
-    method: 'POST',
-    body: JSON.stringify(payload),
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await post(DRIVER_DELIVERY_ACCEPT, payload);
 };
 
 export const forgotPassword = async (email: string) => {
-  const response = await fetch(getUrl(PROFILE_FORGET_PASSWORD), {
-    headers: getHeaders({}),
-    body: JSON.stringify({email: email}),
-    method: 'POST',
-  });
-
-  if (response.ok) {
-    return response;
-  }
-  throw new Exception(response);
+  return await post(PROFILE_FORGET_PASSWORD, {email: email});
 };
 
 export const resendVerificationEmail = async ({email}: {email: string}) => {
-  const response = await fetch(getUrl(MAIL_RESEND), {
-    headers: getHeaders({}),
-    method: 'POST',
-    body: JSON.stringify({email: email}),
-  });
-
-  if (response.ok) {
-    return await response.text();
-  }
-  throw new Exception(response);
+  return await post(MAIL_RESEND, {email: email});
 };
 
 export const changePassword = async (data: {
@@ -266,48 +208,18 @@ export const changePassword = async (data: {
   confirmPassword: string;
   token: string;
 }) => {
-  const response = await fetch(getUrl(PROFILE_NEW_PASSWORD), {
-    headers: getHeaders({}),
-    body: JSON.stringify(data),
-    method: 'POST',
-  });
-
-  if (response.ok) {
-    return response;
-  }
-  throw new Exception(response);
+  return await post(PROFILE_NEW_PASSWORD, data);
 };
 
 export const verifyAccount = async ({token}: {token: string}) => {
-  const response = await fetch(getUrl(MAIL_VERIFICATION), {
-    headers: getHeaders({}),
-    body: JSON.stringify({token: token}),
-    method: 'POST',
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await post(MAIL_VERIFICATION, {token: token});
 };
 
-export const itemReached = async ({
-  itemId,
-  vendorId,
-}: {
+export const itemReached = async (payload: {
   itemId: string;
   vendorId: string;
 }) => {
-  const response = await fetch(getUrl(DRIVER_ITEM_REACHED), {
-    method: 'POST',
-    headers: getHeaders({'auth-token': sharedData.user.token}),
-    body: JSON.stringify({itemId: itemId, vendorId: vendorId}),
-  });
-
-  if (response.ok) {
-    return await response.json();
-  }
-  throw new Exception(response);
+  return await post(DRIVER_ITEM_REACHED, payload);
 };
 
 export class Exception {
